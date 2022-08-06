9fa707c05399f2c82fa1cd42b586af3e811e5aa5
muxammadziz <muxammadazizramziddinov@gmail.com>
2022-08-06 12:29:14 +0500

    getDialogsModel oxirgi xabar bilan olib kelinadigan qilindi

--- src/model/dialogs.model.js
+++ src/model/dialogs.model.js
@@ -5,13 +5,15 @@ const getDialogsModel = async (user_id) => {
         const getUserDialogsQuery = `
         select * from (select * from 
             (
-                select d.*, json_agg(u.*) as companion from dialogs as d 
+                select d.*, json_agg(u.*) as companion, json_agg(m.*) as last_message from dialogs as d 
+                left join  (select * from messages order by created_at desc limit 1) as m 
+                on m.dialog_id = d.dialog_id
                 left join users as u 
                 on array[concat(u.user_id, '')] <@ array[d.dialog_members] and u.user_id != $1
                 group by d.dialog_id
-            )
-            as dialods
-        where array[concat($1, '')] <@ dialog_members) as f left join (select * from messages order by created_at desc limit 1) as r on r.dialog_id = f.dialog_id;
+            ) 
+            as dialogs
+        where array[concat($1, '')] <@ dialog_members) as e;
         `
         return await fetchData(getUserDialogsQuery, user_id)
     } catch (error) {
@@ -24,15 +26,15 @@ module.exports = {
 }
 
 const l = `
-select * from 
-            (
-                select d.*, json_agg(u.*) as companion, m.* from dialogs as d 
-                left join users as u 
-                on array[concat(u.user_id, '')] <@ array[d.dialog_members] and u.user_id != '0912eee5-1b21-4b4e-82c4-af4439be2d03'
-                left join messages as m
-                on d.dialog_id = m.dialog_id 
-                group by d.dialog_id
-            )
-            as dialods
-        where array[concat('0912eee5-1b21-4b4e-82c4-af4439be2d03', '')] <@ dialog_members;
+select * from (select * from 
+    (
+        select d.*, json_agg(u.*) as companion, json_agg(m.*) from dialogs as d 
+        left join (select * from messages order by created_at desc limit 1) as m 
+        on m.dialog_id = d.dialog_id
+        left join users as u 
+        on array[concat(u.user_id, '')] <@ array[d.dialog_members] and u.user_id != '0912eee5-1b21-4b4e-82c4-af4439be2d03'
+        group by d.dialog_id
+    ) 
+    as dialogs
+where array[concat('0912eee5-1b21-4b4e-82c4-af4439be2d03', '')] <@ dialog_members) as e;
 `
